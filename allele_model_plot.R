7ae09a5a070a39da9387f8abbeb23188c19f401e
John Lees <lees.john6@gmail.com>
2015-01-07 14:54:10 +0000

    Add model 1 convergence diagnostics

--- allele_model_plot.R
+++ allele_model_plot.R
@@ -13,7 +13,8 @@ library(plyr)
 # Data input
 data_location <- "~/Documents/PhD/hsd_locus/mapping"
 
-mcmc_file <- paste(data_location, "chain2.Rdata", sep="/")
+mcmc_file1 <- paste(data_location, "chain1.Rdata", sep="/")
+mcmc_file2 <- paste(data_location, "chain2.Rdata", sep="/")
 pairs_file <- paste(data_location, "pair_rows.txt", sep="/")
 
 # Outcomes
@@ -21,7 +22,8 @@ alleles = c("A", "B", "C", "D", "E", "F")
 tissues = c("CSF", "Blood")
 
 # Top level parameters
-parameters = c("mu[1,1]", "mu[2,1]", "mu[1,2]", "mu[2,2]", "mu[1,3]", "mu[2,3]", 
+model1_parameters = c("kappa","mu[1]","mu[2]")
+model2_parameters = c("mu[1,1]", "mu[2,1]", "mu[1,2]", "mu[2,2]", "mu[1,3]", "mu[2,3]",
 	"mu[1,4]", "mu[2,4]", "mu[1,5]", "mu[2,5]", "mu[1,6]", "mu[2,6]")
 
 #
@@ -57,45 +59,69 @@ parameters = c("mu[1,1]", "mu[2,1]", "mu[1,2]", "mu[2,2]", "mu[1,3]", "mu[2,3]",
 #
 
 # Read in data from allele_model.R
-mcmc_samples <- readRDS(mcmc_file)
-all_chains <- as.matrix(mcmc_samples)
+mcmc_samples_model1 <- readRDS(mcmc_file1)
+mcmc_samples_model2 <- readRDS(mcmc_file2)
+
+all_chains1 <- as.matrix(mcmc_samples_model1)
+all_chains2 <- as.matrix(mcmc_samples_model2)
 
 # Pair data
 paired_samples <- read.delim(pairs_file)
 
 # Chain diagnostics plots
+
+# First chain 1
 # Autocorrelation
-pdf("kappa_autocorr.pdf")
-autocorr.plot(mcmc_samples[,c("kappa","mu[1,1]","mu[2,1]"),drop=FALSE])
+pdf("model1_autocorr.pdf")
+autocorr.plot(mcmc_samples_model1[,model1_parameters,drop=FALSE])
 dev.off()
 
 # Chain convergence
-gelman <- gelman.diag(mcmc_samples[,c(parameters,"kappa"),drop=FALSE])
-heidel <- heidel.diag(mcmc_samples[,c(parameters,"kappa"),drop=FALSE])
-capture.output(list("gelman"=gelman,"heidel"=heidel),file="convergence_diagnostics.txt")
+gelman <- gelman.diag(mcmc_samples_model1[,model1_parameters,drop=FALSE])
+heidel <- heidel.diag(mcmc_samples_model1[,model1_parameters,drop=FALSE])
+capture.output(list("gelman"=gelman,"heidel"=heidel),file="model1_convergence_diagnostics.txt")
 
-pdf("kappa_gelman.pdf")
-gelman.plot(mcmc_samples[,"kappa",drop=FALSE])
+pdf("kappa_gelman_model1.pdf")
+gelman.plot(mcmc_samples_model1[,"kappa",drop=FALSE])
 dev.off()
 
 # Plot of kappa + chain, ggplot of kappa
-pdf("kappa_chain.pdf")
-plot(mcmc_samples[,"kappa",drop=F])
+pdf("kappa_chain_model1.pdf")
+plot(mcmc_samples_model1[,"kappa",drop=F])
 dev.off()
 
-kappa_data = as.data.frame(all_chains[,"kappa"])
+# Autocorrelation
+pdf("model2_autocorr.pdf")
+autocorr.plot(mcmc_samples_model2[,c("kappa","mu[1,1]","mu[2,1]"),drop=FALSE])
+dev.off()
+
+# Chain convergence
+gelman <- gelman.diag(mcmc_samples_model2[,c(model2_parameters,"kappa"),drop=FALSE])
+heidel <- heidel.diag(mcmc_samples_model2[,c(model2_parameters,"kappa"),drop=FALSE])
+capture.output(list("gelman"=gelman,"heidel"=heidel),file="model2_convergence_diagnostics.txt")
+
+pdf("kappa_gelman_model2.pdf")
+gelman.plot(mcmc_samples_model2[,"kappa",drop=FALSE])
+dev.off()
+
+# Plot of kappa + chain, ggplot of kappa
+pdf("kappa_chain_model2.pdf")
+plot(mcmc_samples_model2[,"kappa",drop=F])
+dev.off()
+
+kappa_data = as.data.frame(all_chains2[,"kappa"])
 colnames(kappa_data) = "kappa"
 
 pdf("kappa_posterior.pdf")
-ggplot(kappa_data, aes(x=kappa)) + 
-geom_histogram(aes(y=..density..), binwidth=0.025,colour="black",fill="white") + 
+ggplot(kappa_data, aes(x=kappa)) +
+geom_histogram(aes(y=..density..), binwidth=0.025,colour="black",fill="white") +
 geom_density(alpha=0.2, fill="#FF9999")
 dev.off()
 
 # means and 95% HPD intervals, concatenating all chains together
-conf_intervals <- p.interval(all_chains,HPD=TRUE)
+conf_intervals <- p.interval(all_chains2,HPD=TRUE)
 capture.output(conf_intervals,file="confidence_intervals.txt")
-means = colMeans(all_chains)
+means = colMeans(all_chains2)
 capture.output(means,file="means.txt")
 
 # 6*2 means and 95% CIs for mus
@@ -104,19 +130,19 @@ capture.output(means,file="means.txt")
 tissue_alleles <- as.data.frame(rep(alleles,each=length(tissues)))
 colnames(tissue_alleles)[1] = "alleles"
 tissue_alleles$tissue <- rep(tissues,length(alleles))
-tissue_alleles$means = means[parameters]
-tissue_alleles$upper = conf_intervals[parameters,"Upper"]
-tissue_alleles$lower = conf_intervals[parameters,"Lower"]
+tissue_alleles$means = means[model2_parameters]
+tissue_alleles$upper = conf_intervals[model2_parameters,"Upper"]
+tissue_alleles$lower = conf_intervals[model2_parameters,"Lower"]
 
 pdf("tissue_alleles.pdf",width=10,height=7)
-ggplot(tissue_alleles) + 
-geom_pointrange(aes(x=alleles,y=means,ymax=upper,ymin=lower,colour=tissue),position=position_dodge(width=0.9),size=0.8) + 
+ggplot(tissue_alleles) +
+geom_pointrange(aes(x=alleles,y=means,ymax=upper,ymin=lower,colour=tissue),position=position_dodge(width=0.5,size=0.8) +
 xlab("Allele") + ylab("Proportion in tissue") + ylim(0,1) + theme_grey(base_size = 18)
 dev.off()
 
 # 6*1192 means and 95% CIs for pis
 # 6*(1192/2) means and 95% CIs for pi[csf] - pi[blood] for all pairs
-mcmc_diff <- matrix(data=NA,nrow=nrow(all_chains),ncol=(nrow(paired_samples)*length(alleles)))
+mcmc_diff <- matrix(data=NA,nrow=nrow(all_chains2),ncol=(nrow(paired_samples)*length(alleles)))
 col_headers <- vector(mode="character",length=(nrow(paired_samples)*length(alleles)))
 
 for (i in 1:nrow(paired_samples))
@@ -127,11 +153,11 @@ for (i in 1:nrow(paired_samples))
 	for (j in 1:length(alleles))
 	{
 		column_number = (i-1)*length(alleles) + j
-    
+
     csf_allele = paste(csf_sample,j,"]",sep='')
 		blood_allele = paste(blood_sample,j,"]",sep='')
-		
-		mcmc_diff[,column_number] = all_chains[,csf_allele] - all_chains[,blood_allele]
+
+		mcmc_diff[,column_number] = all_chains2[,csf_allele] - all_chains2[,blood_allele]
     col_headers[column_number] = paste(paired_samples$Sample[i],alleles[j],sep='_')
 	}
 }
